adbff72b32672aa2553673bca2703f0aa8ef8d42
Yasha Dayal <yasha.dayal145@gmail.com>
2023-03-16 15:57:02 +0530

    Jar file with dependencies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
 public class Main {
+
+    private static final Logger logger =  (Logger) LogManager.getLogger(Main.class);
+
+    public void fun()
+    {
+        int number = 1;
+        logger.info("[FACTORIAL] - " + number);
+        System.out.println("Inside fun function");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Main m = new Main();
+        m.fun();
+        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Enter first number- ");
+        int a= sc.nextInt();
+        System.out.println("Number is  " +a);
     }
 }
